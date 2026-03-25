04878bc3d3d87f0fb14e7df81fbf4535b47abb09
Enzo <169556594+enzo963@users.noreply.github.com>
2026-03-26 00:30:21 +0300

    Exercise 06- javascript

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    let result = "";
+    if (num < 0) {
+        return "Error";
+    }
+    else if (num === 0) {
+        return '';
+    }
+    for (let i = 0; i < num; i++) {
+        result += string;
+    }
+    return result;
 };
+
+
+console.log(repeatString('enzo', 5));
+console.log(repeatString('enzo', 0));
+console.log(repeatString('enzo', -4));
 
 // Do not edit below this line
 module.exports = repeatString;
