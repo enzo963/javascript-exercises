e242ec76cdc9b1c8375004ed7d26fb8b5c9e65fc
Enzo <169556594+enzo963@users.noreply.github.com>
2026-04-22 02:39:46 +0300

    Solve Exercises

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,46 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let Result = 0;
+  for (let i = 0; i < array.length; i++) {
+    Result += array[i];
+  }
+  return Result;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let Result = 1;
+  for (let i = 0; i < array.length; i++) {
+    Result *= array[i];
+  }
+  return Result;
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function (num1) {
+  if (num1 === 0 || num1 === 1) {
+    return 1;
+  }
+
+  let Result = 1;
+  for (let i = num1; i > 1; i--) {
+    Result *= i;
+  }
+  return Result;
 };
+
+
+
+
 
 // Do not edit below this line
 module.exports = {
